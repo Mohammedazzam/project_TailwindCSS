0da5c541308d2d56c656de57a33ddb3068cecfdf
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-12-28 13:53:33 +0200

    install project all file

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+      body : ['Overpass', 'sans-serif'] ,
+      hero : ['Langar', 'sans-serif'] 
+    },
+    colors: {
+      primary: '#303669'
+    }
+    },
+  
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
