7ef0531cb995e895a1965dda7ea77ded923cf714
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2021-01-02 00:35:11 +0200

    (6 Adding Custom Fonts)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily: {
-      body : ['Overpass', 'sans-serif'] ,
-      hero : ['Langar', 'sans-serif'] 
-    },
-    colors: {
-      primary: '#303669'
+    extend: {},
+    fontFamily: {
+      body : ['Overpass', 'sans-serif']
     }
-    },
-  
   },
   variants: {
     extend: {},
