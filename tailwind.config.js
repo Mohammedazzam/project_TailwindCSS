43e128648283df08a831f7fafed95aa6fe8cb047
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2021-01-02 17:19:16 +0200

    (8 Adding Custom Colors and Fix Custom Font Bug)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    fontFamily: {
-      body : ['Overpass', 'sans-serif'] ,
-      hero : ['Langer' , 'sans-serif']
-    }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    extend: {
+      fontFamily: {
+        body : ['Overpass', 'sans-serif'] ,
+        hero : ['Langar', 'sans-serif'] 
+      },
+      colors: {
+        primary: '#303669'
+      },
+      },
+    
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
